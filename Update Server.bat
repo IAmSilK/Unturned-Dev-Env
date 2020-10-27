eb8cb1a81bfe91c4f892abb2d7831720e37b4f1c
Stephen White <iamsilk3495@gmail.com>
2020-10-27 19:07:54 -0300

    Changed folder name and fixed `mkdir`

--- Update Server.bat
+++ Update Server.bat
@@ -1,5 +1,5 @@
 @echo off
-mkdir "..\Development Server"
+mkdir Unturned
 SteamCMD\steamcmd.exe +login anonymous +force_install_dir "..\Development Server" +app_update 1110390 validate +exit
 
 echo Updated.
