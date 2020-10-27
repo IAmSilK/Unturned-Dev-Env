ba193272f7fc1b970da837eb0905048789535aa3
Stephen White <iamsilk3495@gmail.com>
2020-10-27 19:07:21 -0300

    Change default title

--- Run Server.bat
+++ Run Server.bat
@@ -2,7 +2,7 @@
 cd "Unturned"
 
 :start
-Unturned.exe -batchmode -nographics +secureServer/Unturned Development Server
+Unturned.exe -batchmode -nographics +secureServer/Unturned Server
 
 echo Server shutdown. Press any key to restart or Ctrl + C to exit.
 pause
