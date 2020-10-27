b7b596b456989a4aee3825cab3553c57ff0002b2
Stephen White <iamsilk3495@gmail.com>
2020-10-27 19:08:26 -0300

    Fixed steamcmd install directory

--- Update Server.bat
+++ Update Server.bat
@@ -1,6 +1,6 @@
 @echo off
 mkdir Unturned
-SteamCMD\steamcmd.exe +login anonymous +force_install_dir "..\Development Server" +app_update 1110390 validate +exit
+SteamCMD\steamcmd.exe +login anonymous +force_install_dir "..\Unturned" +app_update 1110390 validate +exit
 
 echo Updated.
 pause
